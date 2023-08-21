c886b43837877c9f982b8e1752569c284475e095
Zhuangpx <571068075@qq.com>
2023-08-21 22:10:33 +0800

    更新rpc

---
+++ api_gateway/rpc/user.go
@@ -0,0 +1,18 @@
+/*
+ * @Autor: violet apricity ( Zhuangpx )
+ * @Date: 2023-08-21 01:24:15
+ * @LastEditors: violet apricity ( Zhuangpx )
+ * @LastEditTime: 2023-08-21 17:56:50
+ * @FilePath: \Road2TikTok\api_gateway\rpc\user.go
+ * @Description:  Zhuangpx : Violet && Apricity:/ The warmth of the sun in the winter /
+ */
+package rpc
+
+import (
+	"github.com/Road-To-Byte/Road2TikTok/api_gateway/rpc/pb"
+	"github.com/gin-gonic/gin"
+)
+
+func UserInfo(ctx *gin.Context, req *pb.UserInfoRequest) (*pb.UserInfoResponse, error) {
+	// return nil, nil
+}
