b135d5303bbf9187858218d4b47a9b81067fc3f1
Zhuangpx <571068075@qq.com>
2023-08-21 22:11:30 +0800

    更新user_service

---
+++ service/user_service/service/init.go
@@ -0,0 +1,9 @@
+/*
+ * @Autor: violet apricity ( Zhuangpx )
+ * @Date: 2023-08-21 02:01:31
+ * @LastEditors: violet apricity ( Zhuangpx )
+ * @LastEditTime: 2023-08-21 17:25:00
+ * @FilePath: \Road2TikTok\service\user_service\service\init.go
+ * @Description:  Zhuangpx : Violet && Apricity:/ The warmth of the sun in the winter /
+ */
+package user_service
