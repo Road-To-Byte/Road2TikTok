3d30433d78c30979403d45f51e04523eaba518f4
Zhuangpx <571068075@qq.com>
2023-08-26 23:36:16 +0800

    Update main.go

--- service/user_service/main.go
+++ service/user_service/main.go
@@ -2,7 +2,7 @@
  * @Autor: violet apricity ( Zhuangpx )
  * @Date: 2023-08-21 01:58:05
  * @LastEditors: violet apricity ( Zhuangpx )
- * @LastEditTime: 2023-08-25 22:27:32
+ * @LastEditTime: 2023-08-26 17:23:00
  * @FilePath: \Road2TikTok\service\user_service\main.go
  * @Description:  Zhuangpx : Violet && Apricity:/ The warmth of the sun in the winter /
  */
@@ -14,10 +14,9 @@ import (
 	"log"
 	"net"
 
-	"github.com/Road-To-Byte/Road2TikTok/api_gateway/rpc/pb"
 	"github.com/Road-To-Byte/Road2TikTok/pkg/etcd"
 	"github.com/Road-To-Byte/Road2TikTok/pkg/viper"
-	"github.com/Road-To-Byte/Road2TikTok/pkg/zap"
+	"github.com/Road-To-Byte/Road2TikTok/service/user_service/pb"
 	user_service "github.com/Road-To-Byte/Road2TikTok/service/user_service/service"
 	"google.golang.org/grpc"
 )
@@ -28,7 +27,6 @@ var (
 	serviceAddr = fmt.Sprintf("%s:%d", config.Viper.GetString("server.host"), config.Viper.GetInt("server.port"))
 	etcdAddr    = fmt.Sprintf("%s:%d", config.Viper.GetString("etcd.host"), config.Viper.GetInt("etcd.port"))
 	signingKey  = config.Viper.GetString("JWT.signingKey")
-	logger      = zap.InitLogger()
 )
 
 func init() {
@@ -36,29 +34,43 @@ func init() {
 }
 
 type server struct {
-	pb.UnimplementedUserServiceServer
+	pb.UnimplementedUserInfoServiceServer
 }
 
 func main() {
-	fmt.Println(etcd.EtcdAddr)
+	log.Println("user_service start")
 	serviceTcpAddr, err := net.ResolveTCPAddr("tcp", serviceAddr)
+	// serviceTcpAddr := "127.0.0.1:6661"
+	// var err error
+	log.Println("1")
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln("service地址解析失败：", err.Error())
 	}
 	conn, err := net.ListenTCP("tcp", serviceTcpAddr)
+	// conn, err := net.Listen("tcp", serviceTcpAddr)
+	log.Println("2")
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln("listen失败：", err.Error())
 	}
 	grpcS := grpc.NewServer()
-	pb.RegisterUserServiceServer(grpcS, &server{})
+	log.Println("3")
+	pb.RegisterUserInfoServiceServer(grpcS, &server{})
+	log.Println("4")
 	//	etcd
 	go etcd.RegisterEndPointToEtcd(context.TODO(), serviceAddr, serviceName)
-	go func() {
-		err := grpcS.Serve(conn)
-		if err != nil {
-			log.Fatalln(err.Error())
-			return
-		}
-	}()
-
+	log.Println("5")
+	log.Println("Waiting for client requests...")
+	serve_err := grpcS.Serve(conn)
+	if serve_err != nil {
+		log.Fatalln(err.Error())
+	}
+	// go func() {
+	// 	log.Println("Waiting for client requests...")
+	// 	err := grpcS.Serve(conn)
+	// 	if err != nil {
+	// 		log.Fatalln(err.Error())
+	// 		return
+	// 	}
+	// }()
+	log.Println("6")
 }
