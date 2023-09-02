492bdca013f333b2e491bd48d0c542583c0fdd80
Zhuangpx <571068075@qq.com>
2023-09-02 23:51:24 +0800

    Update protobuf

---
+++ service/favorite_service/pb/build_pb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+###
+ # @Autor: violet apricity ( Zhuangpx )
+ # @Date: 2023-09-01 14:03:04
+ # @LastEditors: violet apricity ( Zhuangpx )
+ # @LastEditTime: 2023-09-01 15:28:22
+ # @FilePath: \Road2TikTok\service\video_service\pb\build_pb.sh
+ # @Description:  Zhuangpx : Violet && Apricity:/ The warmth of the sun in the winter /
+### 
+
+# 指定输出目录
+output_dir="./"
+
+# 列出所有的 .proto 文件
+proto_files=(
+  "user.proto"
+  "video.proto"
+  "relation.proto"
+  "favorite.proto"
+  "message.proto"
+  "comment.proto"
+)
+
+for proto_file in "${proto_files[@]}"
+do
+  echo "Compiling $proto_file..."
+
+  protoc --proto_path=. --go_out=. --go_opt=paths=source_relative \
+         --go-grpc_out=. --go-grpc_opt=paths=source_relative "$proto_file"
+
+  echo "Compilation of $proto_file completed."
+done
+
+echo "All .proto files compiled successfully."
